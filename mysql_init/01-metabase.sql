df25181334d69ecf2db0c733c988d5f1b5091067
julius <OS20085X@cimbniaga.co.id>
2025-09-15 23:34:27 +0700

    initial

---
+++ mysql_init/01-metabase.sql
@@ -0,0 +1,15 @@
+-- Create Metabase application DB with required charset/collation
+CREATE DATABASE IF NOT EXISTS `metabase`
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Ensure Metabase app user uses mysql_native_password (MariaDB driver compat)
+ALTER USER 'metabase'@'%' IDENTIFIED WITH mysql_native_password BY 'metabase';
+
+-- Metabase app DB needs full privileges
+GRANT ALL PRIVILEGES ON `metabase`.* TO 'metabase'@'%';
+
+-- (Optional) Ensure your analytics DB (created by env) is utf8mb4 as well
+ALTER DATABASE `dashboard`
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+FLUSH PRIVILEGES;
---
+++ readme.txt
@@ -0,0 +1,21 @@
+Create Folder:
+
+mkdir -p /Users/julius/metabase/{mysql_data,metabase_data,plugins,mysql_init}
+
+# set permission read/write/execute
+chmod -R 777 /Users/julius/metabase/mysql_data  
+
+
+
+Start:
+
+cd /Users/julius/metabase_dashboard
+docker compose pull
+docker compose up -d
+
+
+Watch Log:
+
+docker logs -f mysql
+# wait for "ready for connections" and healthcheck to pass
+docker logs -f metabase
