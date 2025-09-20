32b4c0acd89f5541892122e6b0f015a06a8b0b67
julius <OS20085X@cimbniaga.co.id>
2025-09-20 15:25:06 +0700

    some changes

--- mysql_init/01-metabase.sql
+++ mysql_init/01-metabase.sql
@@ -1,15 +1,14 @@
 -- Create Metabase application DB with required charset/collation
-CREATE DATABASE IF NOT EXISTS `metabase`
+CREATE DATABASE IF NOT EXISTS `db_dashboard`
   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Ensure Metabase app user uses mysql_native_password (MariaDB driver compat)
 ALTER USER 'metabase'@'%' IDENTIFIED WITH mysql_native_password BY 'metabase';
 
 -- Metabase app DB needs full privileges
-GRANT ALL PRIVILEGES ON `metabase`.* TO 'metabase'@'%';
+GRANT ALL PRIVILEGES ON `db_dashboard`.* TO 'metabase'@'%';
+FLUSH PRIVILEGES;
 
 -- (Optional) Ensure your analytics DB (created by env) is utf8mb4 as well
-ALTER DATABASE `dashboard`
-  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-FLUSH PRIVILEGES;
+-- ALTER DATABASE `dashboard`
+--  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- readme.txt
+++ readme.txt
@@ -1,21 +1,16 @@
 Create Folder:
 
-mkdir -p /Users/julius/metabase/{mysql_data,metabase_data,plugins,mysql_init}
+mkdir -p /Users/julius/metabase_dashboard/{mysql_data,metabase_data,plugins,mysql_init}
 
 # set permission read/write/execute
-chmod -R 777 /Users/julius/metabase/mysql_data  
-
-
+chmod -R 777 /Users/julius/metabase_dashboard/mysql_data  
 
 Start:
-
 cd /Users/julius/metabase_dashboard
 docker compose pull
 docker compose up -d
 
-
 Watch Log:
-
 docker logs -f mysql
 # wait for "ready for connections" and healthcheck to pass
 docker logs -f metabase
